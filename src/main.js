71bcc97dc9e219edc36b31c1cce2d002046ea61d
matthew-laine <matthew.i.laine@gmail.com>
2019-07-01 10:45:38 -0400

    Get fireworks working a little differently than default

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+import "./main.css"
+import Fireworks from "fireworks-canvas"
+
 export default () => {
-    alert("it works!")
+    const container = document.getElementById("container")
+    const fireworks = new Fireworks(container)
+    const options = {
+        maxRockets:7,
+        rocketSpawnInterval: 100,
+        numParticles: 300,
+        explosionMinHeight: 0.35,
+        explosionMaxHeight: 0.9,
+        explosionChance: 0.4
+    }
+    fireworks.start()
 }
